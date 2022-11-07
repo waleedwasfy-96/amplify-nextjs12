1cafcbfb5b50a8fcd8342ee1f78f854f52be8860
Leejjon <leejjon@gmail.com>
2022-11-07 18:07:16 +0100

    Exclude the deployment stuff in the amplify build

--- deployment/bin/amplify-nextjs13.ts
+++ deployment/bin/amplify-nextjs13.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import {PipelineStack} from "../lib/pipeline-stack";
 

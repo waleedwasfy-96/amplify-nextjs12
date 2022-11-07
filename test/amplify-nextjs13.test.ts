605a01ba6ffedb2316ee6b19b1c597cd03981817
Leejjon <leejjon@gmail.com>
2022-11-07 16:31:14 +0100

    Initial commit

---
+++ test/amplify-nextjs13.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AmplifyNextjs13 from '../lib/amplify-nextjs13-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amplify-nextjs13-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmplifyNextjs13.AmplifyNextjs13Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

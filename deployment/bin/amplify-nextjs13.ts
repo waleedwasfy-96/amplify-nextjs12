f9f0ae6d43da3d197390a830392a6b7e3413ae84
Leejjon <leejjon@gmail.com>
2022-11-07 21:02:08 +0100

    Okay.

--- deployment/bin/amplify-nextjs13.ts
+++ deployment/bin/amplify-nextjs13.ts
@@ -8,7 +8,7 @@ const app = new cdk.App();
 new PipelineStack(app, "amplify-nextjs13-infra-pipeline", {
     codestarArn:
         "arn:aws:codestar-connections:eu-central-1:039085306114:connection/010d9616-2ce5-4d82-a61d-75938f7b0deb",
-    pipelineName: "basic-auth-bug-pipeline",
+    pipelineName: "amplify-pipeline",
     repositoryName: "leejjon/amplify-nextjs13",
     branchName: "main"
 });

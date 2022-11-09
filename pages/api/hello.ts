abc3b850791d36f5909d842e700bb672c74e3b7f
Leejjon <leejjon@gmail.com>
2022-11-09 16:17:59 +0100

    Print cookies and headers in response.

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: string,
+  cookies: string,
+  headers: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const cookies = JSON.stringify(req.cookies);
+  const headers = JSON.stringify(req.headers);
+  res.status(200).json({ name: 'John Doe', cookies, headers})
 }

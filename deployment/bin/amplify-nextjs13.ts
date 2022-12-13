a3df30d9878a304cef46745345be04fe62f3b0e6
waleedwasfy-96 <wasfymow@amazon.com>
2022-12-13 13:12:15 +0000

    Update amplify-nextjs13.ts

--- deployment/bin/amplify-nextjs13.ts
+++ deployment/bin/amplify-nextjs13.ts
@@ -7,7 +7,7 @@ const app = new cdk.App();
 
 new PipelineStack(app, "amplify-nextjs13-infra-pipeline", {
     codestarArn:
-        "arn:aws:codestar-connections:eu-central-1:039085306114:connection/010d9616-2ce5-4d82-a61d-75938f7b0deb",
+        "arn:aws:codestar-connections:us-east-1:037011317234:connection/5edb10a6-c203-4810-88e3-25161f7cb96b",
     pipelineName: "amplify-pipeline",
     repositoryName: "leejjon/amplify-nextjs13",
     branchName: "main"
